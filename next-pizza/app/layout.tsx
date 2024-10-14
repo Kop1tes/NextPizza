1de1ae19b53efa0e0952df57086f1804772e0610
Alexandr Prikhodko <prsasha0@gmail.com>
2024-10-14 16:25:49 +0300

    add header, categories and sortpopup components

--- next-pizza/app/layout.tsx
+++ next-pizza/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import {Nunito} from "next/font/google";
 import "./globals.css";
+import { Header } from "@/components/shared/header";
 
 // const geistSans = localFont({
 //   src: "./fonts/GeistVF.woff",
@@ -32,6 +33,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={nunito.className}>
         <main className="min-h-screen">
+          <Header />
           {children}
         </main>
       </body>
--- next-pizza/app/page.tsx
+++ next-pizza/app/page.tsx
@@ -1,7 +1,16 @@
+import { Container, Title } from "@/components/shared";
+import { Categories } from "@/components/shared/categories";
+import { SortPopup } from "@/components/shared/sort-popup";
+
 export default function Home() {
     return (
-        <h1>
-            Главная
-        </h1>
-    )
+        <>
+            <Container className="mt-5">
+                <Title text="Все пиццы" size="lg" className="font-extrabold" />
+
+                <Categories />
+                <SortPopup />
+            </Container>
+        </>
+    );
 }
