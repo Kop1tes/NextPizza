99ce130057c47cb0c2ed96ba250497a523954fac
Alexandr Prikhodko <prsasha0@gmail.com>
2024-10-16 14:37:50 +0300

    add top-bar and filters components

--- next-pizza/app/page.tsx
+++ next-pizza/app/page.tsx
@@ -1,15 +1,25 @@
-import { Container, Title } from "@/components/shared";
-import { Categories } from "@/components/shared/categories";
-import { SortPopup } from "@/components/shared/sort-popup";
+import { Container, Filters, Title, TopBar } from "@/components/shared";
 
 export default function Home() {
     return (
         <>
             <Container className="mt-5">
                 <Title text="Все пиццы" size="lg" className="font-extrabold" />
-
-                <Categories />
-                <SortPopup />
+            </Container>
+            <TopBar />
+            <Container className="mt-10 pb-14">
+                <div className="flex gap-[60px]">
+                    {/* Фильтрация */}
+                    <div className="w-[250px]">
+                        <Filters />
+                    </div>
+                    {/* Список товаров */}
+                    <div className="flex-1">
+                        <div className="flex flex-col gap-16">
+                            
+                        </div>
+                    </div>
+                </div>
             </Container>
         </>
     );
--- next-pizza/components/shared/index.ts
+++ next-pizza/components/shared/index.ts
@@ -2,3 +2,7 @@ export { Container } from './container';
 export { Header } from './header';
 export { Title } from './title';
 export { Categories } from './categories';
+export { TopBar } from './top-bar';
+export { Filters } from './filters';
+export { FilterCheckbox } from './filter-checkbox';
+export {CheckboxFiltersGroup } from './checkbox-filters-group';
