21b32ec04f00250de9db01142382fad8f8f867bc
Alexandr Prikhodko <prsasha0@gmail.com>
2025-02-19 15:53:23 +0200

    add changes in page.tsx, categories.tsx and top-bar.tsx

--- next-pizza/app/page.tsx
+++ next-pizza/app/page.tsx
@@ -1,14 +1,25 @@
 import { Container, Filters, Title, TopBar } from "@/components/shared";
-import { ProductCard } from "@/components/shared/product-card";
 import { ProductsGroupList } from "@/components/shared/products-group-list";
+import { prisma } from "@/prisma/prisma-client";
 
-export default function Home() {
+export default async function Home() {
+    const categories = await prisma.category.findMany({
+        include: {
+            products: {
+                include: {
+                    ingredients: true,
+                    items: true,
+              }  
+            },
+        }
+    })
+
     return (
         <>
             <Container className="mt-5">
                 <Title text="Все пиццы" size="lg" className="font-extrabold" />
             </Container>
-            <TopBar />
+            <TopBar categories={categories.filter((categories) => categories.products.length > 0)}/>
             <Container className="mt-10 pb-14">
                 <div className="flex gap-[80px]">
                     {/* Фильтрация */}
@@ -18,94 +29,16 @@ export default function Home() {
                     {/* Список товаров */}
                     <div className="flex-1">
                         <div className="flex flex-col gap-16">
-                            <ProductsGroupList title="Пиццы" items={[
-                                {
-                                id: 1,
-                                name: 'Сырная пицца',
-                                imageUrl: 'https://media.dodostatic.com/image/r:292x292/11EEA00D8787476E8D41C6EB1E3EA97D.avif',
-                                price: 550,
-                                items:[{price: 550}],
-                                },
-                                {
-                                id: 2,
-                                name: 'Сырная пицца',
-                                imageUrl: 'https://media.dodostatic.com/image/r:292x292/11EEA00D8787476E8D41C6EB1E3EA97D.avif',
-                                price: 550,
-                                items:[{price: 550}],
-                                },
-                                {
-                                id: 3,
-                                name: 'Сырная пицца',
-                                imageUrl: 'https://media.dodostatic.com/image/r:292x292/11EEA00D8787476E8D41C6EB1E3EA97D.avif',
-                                price: 550,
-                                items:[{price: 550}],
-                                },
-                                {
-                                id: 4,
-                                name: 'Сырная пицца',
-                                imageUrl: 'https://media.dodostatic.com/image/r:292x292/11EEA00D8787476E8D41C6EB1E3EA97D.avif',
-                                price: 550,
-                                items:[{price: 550}],
-                                },
-                                {
-                                id: 5,
-                                name: 'Сырная пицца',
-                                imageUrl: 'https://media.dodostatic.com/image/r:292x292/11EEA00D8787476E8D41C6EB1E3EA97D.avif',
-                                price: 550,
-                                items:[{price: 550}],
-                                },
-                                {
-                                id: 6,
-                                name: 'Сырная пицца',
-                                imageUrl: 'https://media.dodostatic.com/image/r:292x292/11EEA00D8787476E8D41C6EB1E3EA97D.avif',
-                                price: 550,
-                                items:[{price: 550}],
-                                },
-                            ]} categoryId={1} />  
-                            <ProductsGroupList title="Комбо" items={[
-                                {
-                                id: 7,
-                                name: 'Сырная пицца',
-                                imageUrl: 'https://media.dodostatic.com/image/r:292x292/11EEA00D8787476E8D41C6EB1E3EA97D.avif',
-                                price: 550,
-                                items:[{price: 550}],
-                                },
-                                {
-                                id: 8,
-                                name: 'Сырная пицца',
-                                imageUrl: 'https://media.dodostatic.com/image/r:292x292/11EEA00D8787476E8D41C6EB1E3EA97D.avif',
-                                price: 550,
-                                items:[{price: 550}],
-                                },
-                                {
-                                id: 9,
-                                name: 'Сырная пицца',
-                                imageUrl: 'https://media.dodostatic.com/image/r:292x292/11EEA00D8787476E8D41C6EB1E3EA97D.avif',
-                                price: 550,
-                                items:[{price: 550}],
-                                },
-                                {
-                                id: 10,
-                                name: 'Сырная пицца',
-                                imageUrl: 'https://media.dodostatic.com/image/r:292x292/11EEA00D8787476E8D41C6EB1E3EA97D.avif',
-                                price: 550,
-                                items:[{price: 550}],
-                                },
-                                {
-                                id: 11,
-                                name: 'Сырная пицца',
-                                imageUrl: 'https://media.dodostatic.com/image/r:292x292/11EEA00D8787476E8D41C6EB1E3EA97D.avif',
-                                price: 550,
-                                items:[{price: 550}],
-                                },
-                                {
-                                id: 12,
-                                name: 'Сырная пицца',
-                                imageUrl: 'https://media.dodostatic.com/image/r:292x292/11EEA00D8787476E8D41C6EB1E3EA97D.avif',
-                                price: 550,
-                                items:[{price: 550}],
-                                },
-                            ]} categoryId={2} />
+                            {categories.map((category) => (
+                                category.products.length > 0 && (
+                                    <ProductsGroupList
+                                        key={category.id}
+                                        title={category.name}
+                                        categoryId={category.id}
+                                        items={category.products}
+                                    />
+                                ))
+                            )}
                         </div>
                     </div>
                 </div>
