fd17c3d43042e29109a44a57d385f4a71fe99c0a
Alexandr Prikhodko <prsasha0@gmail.com>
2025-04-29 14:38:59 +0300

    modified checkout-personal-form, form-input

--- next-pizza/shared/components/shared/form/form-input.tsx
+++ next-pizza/shared/components/shared/form/form-input.tsx
@@ -23,6 +23,10 @@ export const FormInput: React.FC<Props> = ({ className, name, label, required, .
     const value = watch(name);
     const errorText = errors[name]?.message as string;
 
+    const onClickClear = () => {
+        setValue(name, '', {shouldValidate: true});
+    }
+
     return (
         <div className={className}>
             {label && (
@@ -34,7 +38,7 @@ export const FormInput: React.FC<Props> = ({ className, name, label, required, .
             <div className="relative">
                 <Input className="h-12 text-md" {...register(name)} {...props} />
 
-                {value && <ClearButton />}
+                {value && <ClearButton onClick={onClickClear} />}
             </div>
 
             {errorText && <ErrorText text={errorText} className="mt-2" />}
