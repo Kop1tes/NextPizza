baa56936bfe75f49c0d80f008b76033096a89977
Alexandr Prikhodko <prsasha0@gmail.com>
2026-06-23 18:18:11 +0300

    Fix verify route

--- next-pizza/app/api/auth/verify/route.ts
+++ next-pizza/app/api/auth/verify/route.ts
@@ -1,6 +1,8 @@
 import { prisma } from "@/prisma/prisma-client";
 import { NextRequest, NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(req: NextRequest) {
     try { 
         const code = req.nextUrl.searchParams.get("code");
@@ -38,5 +40,10 @@ export async function GET(req: NextRequest) {
     } catch (error) {
         console.error(error);
         console.log("[VERIFY_GET] Server error", error);
+
+        return NextResponse.json(
+        { error: "Internal server error" },
+        { status: 500 }
+    );
     }
 }
